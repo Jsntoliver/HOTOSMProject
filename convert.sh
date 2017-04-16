1e47fd88940c9f49407cf2332d170c01f679a672
Jsntoliver <jsntoliver@yahoo.com>
2017-04-15 21:10:58 -0500

    Create convert.sh
    Use this script to convert your gpx files into mbtiles.

---
+++ convert.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+
+WORKING_DIR=path_to_desired_dir
+
+cd WORKING_DIR
+
+for i in $( ls path_to_files/*.gpx ); do ogr2ogr path_to_output_storage/gpx.shp -append $i tracks -fieldTypeToString DateTime; done
+
+ogr2ogr -f GeoJSON gpx.json gpx.shp
+
+./tippecanoe -o gpx.mbtiles gpx.json
