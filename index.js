98f51d07100d77429d9478a08e9a7ce6f103503f
Jsntoliver <jsntoliver@yahoo.com>
2017-04-15 21:14:03 -0500

    Create index.js
    Used for comparing mbtiles.

---
+++ index.js
@@ -0,0 +1,24 @@
+var tileReduce = require('tile-reduce');
+var path = require('path');
+var fs = require('fs');
+
+var osm = process.argv[2];
+var tiger = process.argv[3];
+
+if (!osm || !tiger) return console.log('must specify osm and tiger tilesets:', process.argv[0], process.argv[1], '<osm.mbtiles>', '<tiger.mbtiles>');
+
+tileReduce({
+  zoom: 12,
+  map: path.join(__dirname, 'diff.js'),
+  sources: [
+    {name: 'osm',   mbtiles: path.join(__dirname, osm), raw: true},
+    {name: 'tiger', mbtiles: path.join(__dirname, tiger), raw: true}
+  ],
+  output: fs.createWriteStream('difference.ldjson')
+})
+.on('start', function() {
+    console.log('start');
+})
+.on('end', function() {
+    console.log('end');
+});
